724a3665dbe5c61194534c344cb0c9f962d9a20c
Lukeaton <lukeaton@gmail.com>
2018-08-06 21:05:11 +1000

    added title column to articles databse through migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_06_110200) do
+ActiveRecord::Schema.define(version: 2018_08_06_110432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_08_06_110200) do
     t.integer "response_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "title"
   end
 
   create_table "collections", force: :cascade do |t|
